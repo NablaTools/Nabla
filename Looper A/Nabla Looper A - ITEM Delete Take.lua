51e692d9372451a971cceb371d9f739a82837051
NablaTools <esteban@nablatools.com>
2023-08-03 01:37:02 -0600

    Fix Looper A

--- Looper A/Nabla Looper A - ITEM Delete Take.lua
+++ Looper A/Nabla Looper A - ITEM Delete Take.lua
@@ -6,7 +6,10 @@
 * Author URI: http://forum.cockos.com/member.php?u=105939 
 --]] 
 --======================================================================
-console = 0
+local console = 0
+local reaper = reaper
+local proj = proj
+
 function Msg(value, line)
   if console == 1 then
     reaper.ShowConsoleMsg(tostring(value))
@@ -19,196 +22,132 @@ function Msg(value, line)
 end
 
 local info   = debug.getinfo(1,'S');
-script_path  = info.source:match[[^@?(.*[\/])[^\/]-$]]
+local script_path  = info.source:match[[^@?(.*[\/])[^\/]-$]]
 
 reaper.Main_OnCommand(reaper.NamedCommandLookup('_SWS_SAVETRACK'), 0)
 
 local gsub      = string.gsub
 local gmatch    = string.gmatch
 local match     = string.match
-local format    = string.format
 local insert    = table.insert
 local items     = {}
 
-local cItem = reaper.GetSelectedMediaItem(proj, 0)
-
-if not cItem then return reaper.defer(function () end) end
-
-local takes   = {} 
+local codeItem = reaper.GetSelectedMediaItem(proj, 0)
+if not codeItem then return reaper.defer(function () end) end
 local tStrRec = {}
 
-local cTake        = reaper.GetActiveTake( cItem )
+local cTake        = reaper.GetActiveTake( codeItem )
 local name         = reaper.GetTakeName( cTake )
 local subTkName    = match(name, '(.-)%sTK:%d+$')
 local tkName       = subTkName or name
 local sTkName      = tkName:gsub("%s+", "")
-local r, old_chunk = reaper.GetItemStateChunk(cItem, "", false)
+local _, old_chunk = reaper.GetItemStateChunk(codeItem, "", false)
 local fileOrig     = match(old_chunk, '[%\n]FILE%s+[\"](.-)[\"][%\n]')
 
 ------------------------------------------------------------------
 -- FUNCTIONS
 ------------------------------------------------------------------
 local function GetItemType( item, getsectiontype )
-
   local take   = reaper.GetActiveTake(item)
   if not take then return false, "UNKNOW" end
   local source = reaper.GetMediaItemTake_Source(take)
   local type   = reaper.GetMediaSourceType(source, "")
-
   if type ~= "SECTION" then
     return false, type
   else
     if not getsectiontype then
       return true, type
     else
-      local r, chunk     = reaper.GetItemStateChunk(item, "", false)
-      for type in  gmatch(chunk, '<SOURCE%s+(.-)[\r]-[%\n]') do
-        if type ~= "SECTION" then
-          return true, type
+      local _, chunk     = reaper.GetItemStateChunk(item, "", false)
+      for sourceType in  gmatch(chunk, '<SOURCE%s+(.-)[\r]-[%\n]') do
+        if sourceType ~= "SECTION" then
+          return true, sourceType
         end
       end
     end
   end
-  
 end
 
-local function GetItemAction(cItem)
-
-  local r, action     = reaper.GetSetMediaItemInfo_String(cItem, 'P_EXT:ITEM_ACTION', '', false)
-
-  if r then 
-    return action 
-  else
-    old_action = ""
-    local r, isRec      = reaper.GetSetMediaItemInfo_String(cItem, 'P_EXT:ITEM_RECORDING', '', false)
-    local r, isRecMute  = reaper.GetSetMediaItemInfo_String(cItem, 'P_EXT:ITEM_RECMUTE', '', false)
-    local r, isMon      = reaper.GetSetMediaItemInfo_String(cItem, 'P_EXT:ITEM_MON', '', false)
-    if isRec     == "1" then 
-      reaper.GetSetMediaItemInfo_String(cItem, 'P_EXT:ITEM_ACTION', '1', true) 
-      reaper.GetSetMediaItemInfo_String(cItem, 'P_EXT:ITEM_RECORDING', '', true)
-      old_action = "1"
-    else 
-      reaper.GetSetMediaItemInfo_String(cItem, 'P_EXT:ITEM_RECORDING', '', true) 
-    end
-    if isRecMute == "1" then 
-      reaper.GetSetMediaItemInfo_String(cItem, 'P_EXT:ITEM_ACTION', '2', true) 
-      reaper.GetSetMediaItemInfo_String(cItem, 'P_EXT:ITEM_RECMUTE', '', true) 
-      old_action = "2"
-    else 
-      reaper.GetSetMediaItemInfo_String(cItem, 'P_EXT:ITEM_RECMUTE', '', true) 
-    end
-    if isMon     == "1" then 
-      reaper.GetSetMediaItemInfo_String(cItem, 'P_EXT:ITEM_ACTION', '3', true) 
-      reaper.GetSetMediaItemInfo_String(cItem, 'P_EXT:ITEM_MON', '', true) 
-      old_action = "3"
-    else 
-      reaper.GetSetMediaItemInfo_String(cItem, 'P_EXT:ITEM_MON', '', true) 
-    end
-    return old_action
+local function GetItemAction(codeItem)
+  local r, action     = reaper.GetSetMediaItemInfo_String(codeItem, 'P_EXT:ITEM_ACTION', '', false)
+  if r then
+    return action
   end
 end
 
-local function CreateTableAllItems()
-
-  local count = reaper.CountMediaItems( proj )
-
-  for i = 0, count - 1 do
-
-    local cItem       = reaper.GetMediaItem(proj, i)
-    local _, type         = GetItemType( cItem, true )
-    
-    if type ~= "UNKNOW" and type ~= "RPP_PROJECT" and type ~= "VIDEO" and type ~= "CLICK" and type ~= "LTC" and type ~= "VIDEOEFFECT" then
-
-      local cTake       = reaper.GetMediaItemTake(cItem, 0)
-      local name        = reaper.GetTakeName( cTake )
-      local subTkName   = match(name, '(.-)%sTK:%d+$')
-      local tkName      = subTkName or name
-      local tkIdx       = match(name, '%d+$')
-      local iLock       = reaper.GetMediaItemInfo_Value(cItem,"C_LOCK")
-      local iLen        = reaper.GetMediaItemInfo_Value(cItem,"D_LENGTH")
-      local lenQN       = reaper.TimeMap2_timeToQN( proj, iLen ) * 960
-      local action        = GetItemAction(cItem)
-      local source         = reaper.GetMediaItemTake_Source(cTake)
-      local _, _, _, mode = reaper.PCM_Source_GetSectionInfo( source )
-
-      items[#items+1] = { 
-        cItem       = cItem, 
-        cTake       = cTake, 
-        tkName      = tkName ,
-        tkIdx       = tkIdx,
-        iLock       = iLock ,
-        isRecord    = isRecord,
-        isRecMute   = isRecMute,
-        lenQN       = lenQN,
-        type        = type,
-        mode        = mode,
-      }
-
+local function CheckIfItemTypeIsAudioOrMIDI(item_type)
+    local forbidenTypes = {"UNKNOW", "RPP_PROJECT", "VIDEO", "CLICK", "LTC", "VIDEOEFFECT"}
+    for i = 0, #forbidenTypes do
+        if forbidenTypes[i] == item_type then
+            return false
+        end
     end
+    return true
+end
 
+local function CreateTableAllItems()
+  local count = reaper.CountMediaItems( proj )
+  for i = 0, count - 1 do
+    local codeItem       = reaper.GetMediaItem(proj, i)
+    local _, item_type         = GetItemType( codeItem, true )
+    if not CheckIfItemTypeIsAudioOrMIDI(item_type) then goto next end
+    local cTake       = reaper.GetMediaItemTake(codeItem, 0)
+    local name        = reaper.GetTakeName( cTake )
+    local subTkName   = match(name, '(.-)%sTK:%d+$')
+    local tkName      = subTkName or name
+    local tkIdx       = match(name, '%d+$')
+    local iLock       = reaper.GetMediaItemInfo_Value(codeItem,"C_LOCK")
+    local iLen        = reaper.GetMediaItemInfo_Value(codeItem,"D_LENGTH")
+    local lenQN       = reaper.TimeMap2_timeToQN( proj, iLen ) * 960
+    local source         = reaper.GetMediaItemTake_Source(cTake)
+    local _, _, _, mode = reaper.PCM_Source_GetSectionInfo( source )
+    items[#items+1] = {
+      codeItem       = codeItem,
+      cTake       = cTake,
+      tkName      = tkName ,
+      tkIdx       = tkIdx,
+      iLock       = iLock ,
+      lenQN       = lenQN,
+      type        = type,
+      mode        = mode,
+    }
+    ::next::
   end
 end
 
 function PropagateAudio(sTkName, source, oldSoffs, oldLen )
-
   for i = 1, #items do
-
     local v = items[i]
-
-      if v.tkName == sTkName then
-
-        if v.iLock ~= 1 then
-
-          local r, iChunk = reaper.GetItemStateChunk(v.cItem, "", false) -- coppy item chunk
-
-          if v.mode then
-
-            -- local new_chunk = gsub(iChunk, '<SOURCE%s+.->', "<SOURCE SECTION\n".."LENGTH "..oldLen.."\n".."STARTPOS "..oldSoffs.."\n".."MODE  ".."2".."\n".."<SOURCE WAVE\n".."FILE \""..oldSource.."\"".."\n>\n", 1)
-            -- reaper.SetItemStateChunk(v.cItem, new_chunk, true) 
-            -- reaper.GetSetMediaItemTakeInfo_String( v.cTake, 'P_NAME', v.tkName, true )
-            ------------------------------------------------------------------
-            reaper.BR_SetTakeSourceFromFile2( v.cTake, source, false, true )
-            reaper.GetSetMediaItemTakeInfo_String( v.cTake, 'P_NAME', v.tkName, true )
-            reaper.BR_SetMediaSourceProperties( v.cTake, true, 0, oldLen, 0, true )
-            -- reaper.SetMediaItemTakeInfo_Value( v.cTake, 'D_PLAYRATE', 1 )
-
-          else
-
-            -- local str = "<SOURCE SECTION\n".."LENGTH "..oldLen.."\n".."STARTPOS "..oldSoffs.."\n".."<SOURCE WAVE\n".."FILE "..oldSource.."\n>\n"
-            -- local new_chunk = gsub(iChunk, '<SOURCE%s+.->', str, 1)
-            -- reaper.SetItemStateChunk(v.cItem, new_chunk, true) 
-            -- reaper.GetSetMediaItemTakeInfo_String( v.cTake, 'P_NAME', v.tkName, true )
-            ------------------------------------------------------------------
-            reaper.BR_SetTakeSourceFromFile2( v.cTake, source, false, true )
-            reaper.GetSetMediaItemTakeInfo_String( v.cTake, 'P_NAME', v.tkName, true )
-            -- reaper.SetMediaItemTakeInfo_Value( v.cTake, 'D_PLAYRATE', 1 )
-
-          end
-        end      
+    if v.tkName == sTkName then
+      if v.iLock ~= 1 then
+        if v.mode then
+          reaper.BR_SetTakeSourceFromFile2( v.cTake, source, false, true )
+          reaper.GetSetMediaItemTakeInfo_String( v.cTake, 'P_NAME', v.tkName, true )
+          reaper.BR_SetMediaSourceProperties( v.cTake, true, 0, oldLen, 0, true )
+        else
+          reaper.BR_SetTakeSourceFromFile2( v.cTake, source, false, true )
+          reaper.GetSetMediaItemTakeInfo_String( v.cTake, 'P_NAME', v.tkName, true )
+        end
       end
+    end
   end
   reaper.Main_OnCommand(40047, 0)
   reaper.UpdateArrange()
 end
 
 function PropagateMIDI(tkName, key)
-
   for i = 1, #items do
-
     local v = items[i]
-
     if v.tkName == tkName then
-
-      local _, chunk    = reaper.GetItemStateChunk(v.cItem, '', false)
-
+      local _, chunk    = reaper.GetItemStateChunk(v.codeItem, '', false)
       if v.type == "MIDI" then
-         local new_chunk   = gsub(chunk, '<SOURCE%s+.->[\r]-[%\n]', "<SOURCE MIDI\n".."E "..v.lenQN.." b0 7b 00".."\n>\n", 1)
-         reaper.SetItemStateChunk(v.cItem, new_chunk, true) 
-         reaper.GetSetMediaItemTakeInfo_String( v.cTake, 'P_NAME', tkName, true )
+        local new_chunk   = gsub(chunk, '<SOURCE%s+.->[\r]-[%\n]', "<SOURCE MIDI\n".."E "..v.lenQN.." b0 7b 00".."\n>\n", 1)
+        reaper.SetItemStateChunk(v.codeItem, new_chunk, true) 
+        reaper.GetSetMediaItemTakeInfo_String( v.cTake, 'P_NAME', tkName, true )
       elseif v.type == "MIDIPOOL" then
-         local new_chunk   = gsub(chunk, '<SOURCE%s+.->[\r]-[%\n]', "<SOURCE MIDIPOOL\n".."E "..v.lenQN.." b0 7b 00".."\n>\n", 1)
-         reaper.SetItemStateChunk(v.cItem, new_chunk, true) 
-         reaper.GetSetMediaItemTakeInfo_String( v.cTake, 'P_NAME', tkName, true )
+        local new_chunk   = gsub(chunk, '<SOURCE%s+.->[\r]-[%\n]', "<SOURCE MIDIPOOL\n".."E "..v.lenQN.." b0 7b 00".."\n>\n", 1)
+        reaper.SetItemStateChunk(v.codeItem, new_chunk, true) 
+        reaper.GetSetMediaItemTakeInfo_String( v.cTake, 'P_NAME', tkName, true )
       end
 
     end
@@ -216,43 +155,32 @@ function PropagateMIDI(tkName, key)
 end
 
 function GetNumForLoopTakes( cTake )
-
   for i = 0, 500 do
-
     retval, key, val = reaper.EnumProjExtState( 0, cTake, i )
     if retval == false then return i+1 end
     takes[i+1] = val
-
   end
-
 end
 
 function drawMenu(menuName)
-
   local SHIFT_X = -50;
-    local SHIFT_Y = 15;
+  local SHIFT_Y = 15;
   local x,y = reaper.GetMousePosition();
   local x,y =  x+(SHIFT_X or 0),y+(SHIFT_Y or 0);
-
   gfx.init('',0,0,0,x,y)
-  
-------------------------------------------------------------------
-
   local str = ''
   local str_2 = ''
-
-  for i = 0, 1000 do
+  for i = 0, 10000 do
     retval, key, val = reaper.EnumProjExtState( 0, sTkName, i )
     if retval == false then  break end
     str = str.."|Delete - "..tkName.." - Take "..string.format("%d", key)
   end
-
   retval = gfx.showmenu(str)
 
   if retval > 0 then
     _, key, str_val = reaper.EnumProjExtState( 0, sTkName, string.format("%.0f", retval-1 ) )
     for substring in gmatch(str_val, '([^,]+)') do insert(tStrRec, substring) end
-      yesno =  reaper.MB( "Delete \""..tkName.." TK:"..string.format("%.0f", key ).."\"".." and its media file: "..tStrRec[1], "Delete take and source files (no undo!)", 4 )
+    yesno =  reaper.MB( "Delete \""..tkName.." TK:"..string.format("%.0f", key ).."\"".." and its media file: "..tStrRec[1], "Delete take and source files (no undo!)", 4 )
     if yesno == 6 then
 
       if tStrRec[4] == "AUDIO" then
